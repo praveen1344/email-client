16f29eaa3e5a5e008be0e3059ac298125b503bc7
Praveen K Venkatesan <praveenmanu23@gmail.com>
2019-03-10 18:15:06 -0500

    Made minor updates to feed, included material ui package

--- src/feed/feed.js
+++ src/feed/feed.js
@@ -11,18 +11,27 @@ class MailFeed extends Component {
         feedData: [],
         isLoaded: false
     };
+
+    this.checkboxIdentifier = this.checkboxIdentifier.bind(this);
   }
 
   componentDidMount(){
     fetch("https://5c5a21f9af3ff700140de477.mockapi.io/api/email")
       .then(res => res.json())
       .then((res) => {
+        res.map((item)=>(
+          item.isChecked = true
+        ));
         this.setState({
           isLoaded: true,
           feedData: res,
           error: false
         });
+        console.log(this.state.feedData)
       })
+  }
+  checkboxIdentifier(identifier){
+    return 'mailfeed-checkbox-' + identifier;
   }
   render(){
     if(!this.state.isLoaded){
@@ -33,8 +42,13 @@ class MailFeed extends Component {
         <ul className="feed-container">
           {
             this.state.feedData.map(item => (
-                <li id={item.id}>
-                  {item.subject}
+                <li id={item.id} className='feed-mail-item'>
+                  <div className="feed-mail-checkbox">
+                    <label for={this.checkboxIdentifier(item.id)}></label>
+                    <input type="checkbox" name="mailFeed" value={item.isChecked} id={this.checkboxIdentifier(item.id)}/>
+                  </div>
+                  <div className="feed-mail-sender">{item.from}</div>
+                  <div className="feed-mail-subj">{item.subject}</div>
                 </li>
             ))
           }
--- src/loader.js
+++ src/loader.js
@@ -5,7 +5,7 @@ class Loader extends Component{
   render(){
     return (
       <div className="loader">
-        <svg width="200px"  height="200px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-eclipse" style={{background: "none"}}><path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#8cd0e5" transform="rotate(311.944 50 51)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></path></svg>
+        <svg width="200px"  height="200px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="lds-eclipse" style={{background: "none"}}><path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#8cd0e5" transform="rotate(311.944 50 51)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></path></svg>
       </div>
     )
   }
